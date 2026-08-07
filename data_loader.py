1e2045f7287a65a2bf6bab277a32ae8e22e65b3c
anthonyliao77 <anthony.liao77@gmail.com>
2026-08-07 03:46:54 +0200

    Data loader now loads in new dataframe with optimal features

--- data_loader.py
+++ data_loader.py
@@ -4,6 +4,7 @@ Loads and combines football match data from dataset.
 
 import glob
 import pandas as pd
+from features import create_features
 
 # Choose football league
 files_path = glob.glob("LaLiga/*.csv")
@@ -19,3 +20,6 @@ dataframe = pd.concat(files_list, ignore_index=True)
 # Convert date to timestamps
 dataframe["Date"] = pd.to_datetime(dataframe["Date"], dayfirst=True)
 dataframe = dataframe.sort_values("Date").reset_index(drop=True)
+
+# Add new features to dataframe
+dataframe = create_features(dataframe=dataframe)
