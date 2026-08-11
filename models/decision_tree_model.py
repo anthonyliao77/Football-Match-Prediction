8ed0b6135e18f3a34d30f20642eeef5147919728
anthonyliao77 <anthony.liao77@gmail.com>
2026-08-11 21:20:49 +0200

    Refactor

--- models/decision_tree_model.py
+++ models/decision_tree_model.py
@@ -26,8 +26,11 @@ dataframe = pd.concat(files_list, ignore_index=True)
 # Predict 25/26 season
 
 dataframe["Date"] = pd.to_datetime(dataframe["Date"], dayfirst=True)
-train_data = dataframe[dataframe["Date"]<= "31-05-2025"]
-validation_data = dataframe[(dataframe["Date"] >= "10-08-2025") & (dataframe["Date"] <= "31-05-26")]
+train_data = dataframe[dataframe["Date"] <= "31-05-2025"]
+validation_data = dataframe[
+    (dataframe["Date"] >= "10-08-2025")
+    & (dataframe["Date"] <= "31-05-26")
+]
 
 # select important cols in dataset
 cols = [
