83e8f77abe06e2c0b7701e13587a0913c4ab4858
anthonyliao77 <anthony.liao77@gmail.com>
2026-08-18 22:29:54 +0200

    Added fixed dates for training and validation data

--- config.py
+++ config.py
@@ -2,13 +2,17 @@
 Stores feature definitions for the evaluation model.
 """
 
+TRAIN_END_DATE = "2025-05-31"
+VALIDATION_START_DATE = "2025-08-15"
+VALIDATION_END_DATE = "2026-06-01"
+
 feature_columns = [
     # Team points last five matches
     "HomePT5",
     "AwayPT5",
     # Team goals scored last five matches
     "HomeGS5",
-    "AwayGD5",
+    "AwayGS5",
     # Team goals conceded last five matches
     "HomeGC5",
     "AwayGC5",
