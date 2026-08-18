d26217a9428c83273ba4067384e2311dc5b8bf32
anthonyliao77 <anthony.liao77@gmail.com>
2026-08-18 22:29:06 +0200

    Changed load_data function to purely reading raw data

--- data_loader.py
+++ data_loader.py
@@ -6,8 +6,6 @@ import glob
 
 import pandas as pd
 
-from features import create_features
-
 
 def load_data(league: str) -> pd.DataFrame:
     '''
@@ -16,11 +14,12 @@ def load_data(league: str) -> pd.DataFrame:
     Parameters:
         league (str): The name of the football league to load data for.
     Returns:
-        pd.DataFrame: A DataFrame containing the combined match data for
+        pd.DataFrame: A DataFrame containing the raw combined match data for
         the specified league.
     '''
     files_path = glob.glob(f"{league}/*.csv")
 
+    # Read dataset
     files_list = []
     for file in files_path:
         files_list.append(pd.read_csv(file))
@@ -28,8 +27,5 @@ def load_data(league: str) -> pd.DataFrame:
     dataframe = pd.concat(files_list, ignore_index=True)
     dataframe["Date"] = pd.to_datetime(dataframe["Date"], dayfirst=True)
     dataframe = dataframe.sort_values("Date").reset_index(drop=True)
-
-    # Add new features to dataframe
-    dataframe = create_features(dataframe=dataframe)
 
     return dataframe
