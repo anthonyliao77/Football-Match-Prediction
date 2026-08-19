0a0bc9bd518586ac7d85988160ea0fed1c43ea03
anthonyliao77 <anthony.liao77@gmail.com>
2026-08-19 23:44:54 +0200

    Refactor league configuration and data loading

--- train.py
+++ train.py
@@ -7,6 +7,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix
 
 from config import (
+    LEAGUES,
     TRAIN_END_DATE,
     VALIDATION_END_DATE,
     VALIDATION_START_DATE,
@@ -14,14 +15,40 @@ from config import (
 )
 from data_loader import load_data
 from features import create_features
+from understat_loader import load_understat_data
 
 # Choosen league to train and evaluate the model on
-LEAGUE = "Football-data/SerieA"
+LEAGUE = "LaLiga"
+LEAGUE_CONFIG = LEAGUES[LEAGUE]
 
 # Load and combine match data for the specified league
-dataframe = load_data(league=LEAGUE)
+dataframe = load_data(
+    league=LEAGUE_CONFIG["football_data"]
+)
+
+# Load xg data from understat for the specified league and season range
+xg_data = load_understat_data(
+    league=LEAGUE_CONFIG["understat"],
+    start_year=2020,
+    end_year=2025
+)
+
+# Merge the match data and xg data based on date and team names
+dataframe = pd.merge_asof(
+    dataframe,
+    xg_data,
+    left_on="Date",
+    right_on="date",
+    left_by=["HomeTeam", "AwayTeam"],
+    right_by=["home_team", "away_team"],
+    direction="nearest",
+    tolerance=pd.Timedelta(days=1)
+)
+
+# Create features for the model using the combined match data
 dataframe = create_features(dataframe=dataframe)
 
+# Split the data into training and validation sets based on date ranges
 train_data = dataframe[dataframe["Date"] <= TRAIN_END_DATE]
 validation_data = dataframe[
     (dataframe["Date"] >= VALIDATION_START_DATE)
